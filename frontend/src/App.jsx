9fc9a810ad8d606e851551539ac03f203dc15908
linyubing <yul201@ucsd.edu>
2024-05-10 23:59:52 -0700

    signUpPage implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import SignInPage from './SignInPage.jsx';
+import SignUpPage from './SignUpPage.jsx';
 
 function App() {
   return (
-        <SignInPage />
+        <SignUpPage />
   );
 }
 
