93e283f4cafaca9ba56a349a38ccb3d4a641393f
ceciliatruong <114256675+ceciliatruong@users.noreply.github.com>
2024-05-12 14:35:09 -0700

    implemented skeleton navbar & added page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import SignInPage from './SignInPage.jsx'
+import Home from './pages/Home/index.jsx';
+import { Routes, Route } from 'react-router-dom'
+import Discover from './pages/Discover/index.jsx';
+import Settings from './pages/Settings/index.jsx';
+import SignIn from './pages/SignIn/index.jsx';
 
 function App() {
   return (
     <div className="App">
-        <SignInPage />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/discover" element={<Discover />} />
+          <Route path="/settings" element={<Settings/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="*" element={<h1>page not found</h1>}/>
+        </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/navbar/index.jsx
@@ -0,0 +1,33 @@
+import './NavBar.css'
+import { Outlet, Link } from 'react-router-dom'
+export default function NavBar() {
+    return(
+        <nav className='nav-bar'>
+            <div className='logo'>
+                <img src="../../public/images/nav-logo.png" alt='Food Journal Logo'/>
+                <p>My Food Journal</p>
+            </div>
+            <ul>
+                <li>
+                    <Link to='/'>HOME</Link>
+                </li>
+                <li>
+                    <Link to='/discover'>DISCOVER</Link>
+                </li>
+                <li>
+                    <Link to='/settings'>SETTINGS</Link>
+                </li>
+            </ul>
+            <div className='sign-in'>
+                <ul>
+                    <li>
+                        <img src='../../public/images/default-pfp.png' alt='Default Profile Picture'/>
+                    </li>
+                    <li>
+                    <Link to='/signin'>Sign in?</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
