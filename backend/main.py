01fe4755855eb509ed3e789bf9d259a8f4663ae2
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2024-05-16 14:40:41 -0700

    modified main.py

--- backend/main.py
+++ backend/main.py
@@ -23,5 +23,5 @@ def root():
 
 app.include_router(router_auth)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=6542, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=6542, reload=True)
