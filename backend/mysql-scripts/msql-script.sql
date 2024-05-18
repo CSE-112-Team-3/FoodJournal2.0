09594c646b27f2899acec09edb63e28ab1b8f025
Marc <mreta0002@gmail.com>
2024-05-17 17:55:34 -0700

    added post table

--- backend/mysql-scripts/msql-script.sql
+++ backend/mysql-scripts/msql-script.sql
@@ -15,9 +15,10 @@ CREATE TABLE if not exists fd_users (
 CREATE TABLE if not exists post (
   id int primary key,
   food_name varchar(64) not null,
+  img varchar(64),
   rating int not null,
-  username varchar(64) not null,
-  password varchar(64) not null,
-  email varchar(64),
+  rest_name varchar(64),
+  review varchar(64) not null,
+  tags varchar(64) not null,
   FOREIGN KEY (id) REFERENCES fd_users(id)
 );
