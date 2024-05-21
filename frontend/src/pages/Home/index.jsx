1d77af7eb06ef0ef55ee2bb907688a936c1fa47c
linyubing <yul201@ucsd.edu>
2024-05-21 14:01:27 -0700

    nav from home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import './Home.css'
 import NavBar from '../../components/navbar'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return(
         <div className="container">
             <NavBar/>
-            <button className="circle-btn">+</button>
+            <Link to="/new-review">
+              <button className="circle-btn">+</button>
+            </Link>
         </div>
         
     );
