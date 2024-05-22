ac973bce6bd8b4d4bce6a85208c0b23eccf25d39
ceciliatruong <114256675+ceciliatruong@users.noreply.github.com>
2024-05-22 01:20:12 -0700

    completed basic minimized post component

--- frontend/src/components/minimizedPost/index.jsx
+++ frontend/src/components/minimizedPost/index.jsx
@@ -1,12 +1,23 @@
-import './Post.css'
+import './minimizedPost.css'
+
 export default function MinimizedPost({ username, profilePic, mealName, starRating, description, images }) {
     return(
         <div className='min-post-container'>
-            <img src={profilePic}/>
-            <p>@{username} just ate {mealName}</p>
-            {starRating}
-            <img className='min-post-image' src={images[0]} width='300px' height='300px'/>
-            <p>{description}</p>
+            <div className='profile-section'>
+                {profilePic}
+                <div className='user-info'>
+                    <p className='username'>@{username}</p>
+                    <p className='meal-name'>just ate {mealName}</p>
+                    <div className='star-rating'>{starRating}</div>
+                </div>
+            </div>
+            <div className='content-section'>
+                <img className='min-post-image' src={images[0]} alt={mealName} />
+                <p className='description'>{description}</p>
+            </div>
+            <div className='interact-bar'>
+                {/* to add like button here in the future? */}
+            </div>
         </div>
     );
 }
