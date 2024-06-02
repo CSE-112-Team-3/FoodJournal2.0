309273293719c3d10e6a30ac913c44405874dfa6
adhithiganesan <103284260+adhithiganesan@users.noreply.github.com>
2024-06-02 12:03:35 -0700

    added buttons, api call not working :(

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 import Home from './pages/Home/index.jsx';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Recipes from './pages/Recipes/index.jsx';
 import Settings from './pages/Settings/index.jsx';
 import SignIn from './pages/SignIn/index.jsx';
 import SignUp from './pages/SignUp/index.jsx';
 import NavBar from './components/navbar/index.jsx';
 import Profile from './pages/Profile/index.jsx';
-import Review from './pages/Review/index.jsx';
+import ReviewPage from './pages/Review/index.jsx';
+import MinimizedPost from './components/minimizedPost/index.jsx';
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/settings" element={<Settings/>}/>
-          <Route path="/signin" element={<SignIn/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/new-review" element={<Review/>}/>
-          <Route path="*" element={<h1>page not found</h1>}/>
-
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/new-review" element={<ReviewPage/>}/>
+        <Route path="/minimizedpost" element={<MinimizedPost />} />
+        <Route path="/review" element={<ReviewPage />} />
+        <Route path="*" element={<h1>page not found</h1>}/>
+      </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
--- frontend/src/components/minimizedPost/index.jsx
+++ frontend/src/components/minimizedPost/index.jsx
@@ -1,26 +1,33 @@
-import './minimizedPost.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './minimizedPost.css';
 
 export default function MinimizedPost({ username, profilePic, mealName, starRating, description, images }) {
-    return(
+    const navigate = useNavigate();
+
+    const handleUpdateClick = () => {
+        const imageUrls = images.map(image => image.src || image);
+        const state = { username, mealName, starRating, description, images: imageUrls };
+        navigate('/review', { state });
+    };
+
+    return (
         <div className='min-post-container'>
             <div className='profile-section'>
                 {profilePic}
                 <div className='user-info'>
                     <p className='username'>@{username}</p>
                     <p className='meal-name'>just ate {mealName}</p>
-                    <div className='star-rating'>{starRating}</div>
+                    <div className='star-rating'>{'★'.repeat(starRating)}</div>
                 </div>
             </div>
             <div className='content-section'>
-                <img className='min-post-image' src={images[0]} alt={mealName} />
+                {images && images[0] && <img className='min-post-image' src={images[0].src || images[0]} alt={mealName} />}
                 <p className='description'>{description}</p>
             </div>
-            <div className = 'postActionButtons'>
-                    <button className='update-button'>Update Post</button>
-                    <button className='delete-button'>Delete Post</button>
-            </div>
-            <div className='interact-bar'>
-                {/* to add like button here in the future? */}
+            <div className='postActionButtons'>
+                <button className='update-button' onClick={handleUpdateClick}>Update Post</button>
+                <button className='delete-button'>Delete Post</button>
             </div>
         </div>
     );
