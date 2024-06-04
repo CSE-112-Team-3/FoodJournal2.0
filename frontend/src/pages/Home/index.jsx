1092a89f4b4c070e6933d41736024f2cf8b1f1d5
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2024-06-03 22:36:25 -0700

    modified home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,3 +1,55 @@
+
+// import './Home.css'
+// import NavBar from '../../components/navbar'
+// import React, { useState, useEffect } from 'react';
+
+// import MinimizedPost from '../../components/minimizedPost';
+// import StaticStarRating from '../../components/staticStarRating';
+// import ProfilePic from '../../components/profilePic';
+// import { Link } from 'react-router-dom'
+
+// export default function Home() {
+//     const [pictureNavbar, setPictureNavbar] = useState('http://ssl.gstatic.com/accounts/ui/avatar_2x.png'); 
+//     const [posts, setPosts] = useState([]);
+
+//     useEffect(() => {
+//         // const url = 'https://foodjournal20-production.up.railway.app/api/v1/post_review/get_post_review';
+//         const url = 'http://127.0.0.1:6542/api/v1/post_review/get_post_review';
+//         fetch(url, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             setPosts(data);
+//             console.log(data);
+//         })
+//         .catch(error => console.error('Error fetching data:', error));
+//     }, []);
+
+//     return (
+//         <div className='home-container'>
+//             <NavBar pictureNavbar={pictureNavbar} />
+//             <div className='post-container reddit-sans-condensed'>
+//                 {posts.length > 0 ? posts.map(post => (
+//                     <MinimizedPost
+//                         key={post.id}
+//                         id={post.id}
+//                         username={post.username}
+//                         profilePic={<ProfilePic username={post.username} imageAddress={post.profile_pic} size='100px' />}
+//                         mealName={post.food_name}
+//                         starRating={<StaticStarRating rating={post.rating} />}
+//                         description={post.review}
+//                         images={post.image}
+//                     />
+//                 )) : <p>No posts available.</p>}
+//             </div>
+//         </div>
+//     );
+// };
+
 import './Home.css'
 import NavBar from '../../components/navbar'
 import { useState } from 'react';
@@ -9,6 +61,7 @@ import { Link } from 'react-router-dom'
 
 
 export default function Home() {
+    const [pictureNavbar, setPictureNavbar] = useState('http://ssl.gstatic.com/accounts/ui/avatar_2x.png');
     //TODO: Retrieve the image link from the database to replace the link in 'useState'.
     const posts = [
         {
@@ -40,7 +93,7 @@ export default function Home() {
 
     return(
         <div className='home-container'>
-            <NavBar/>
+            <NavBar pictureNavbar={pictureNavbar} />
             <div className='post-container reddit-sans-condensed'>
                 {posts ? posts.map((post, index) => (
                 <MinimizedPost
