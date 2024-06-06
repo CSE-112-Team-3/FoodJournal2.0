be1374d0195b8e2ccfa943e8ab1592b06cfda965
linyubing <yul201@ucsd.edu>
2024-06-06 14:33:01 -0700

    review detail done

--- frontend/src/pages/ReviewDetail/index.jsx
+++ frontend/src/pages/ReviewDetail/index.jsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './ReviewDetail.css';
 
 import StaticStarRating from '../../components/staticStarRating';
-import ProfilePic from '../../components/profilePic';
+import backgroundImage from '../../assets/background.jpg';
 
 const ReviewDetail = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const { post } = location.state;
+
+    useEffect(() => {
+        document.body.style.backgroundImage = `url(${backgroundImage})`;
+        document.body.style.backgroundSize = 'cover';
+    
+        return () => {
+          document.body.style.backgroundImage = '';
+          document.body.style.backgroundSize = '';
+        };
+      }, [])
+
+    const handleReturnClick = () => {
+        navigate('/');
+    };
 
     return (
         <div className='review-detail-container'>
-            <h1>{post.food_name}</h1>
-            <div className='review-detail'>
-                <img src={post.profile_pic} alt={`${post.username}'s profile`} className='profile-pic'/>
-                <h2>{post.username}</h2>
-                <div className='star-rating'>
-                    <StaticStarRating rating={post.rating} />
-                </div>
-                <p>{post.review}</p>
-                {post.image && <img src={post.image} alt={`${post.food_name}`} className='food-pic' />}
-                <p><strong>Restaurant:</strong> {post.restaurant_name}</p>
+            <div className='review-detail-header'>
+                <img src={post.profile_pic} alt={`${post.username}'s profile`} className='profile-pic' />
+                <span className='username'>{post.username}</span>
+            </div>
+            {post.image && post.image.trim() !== '' && <img src={post.image} alt={`${post.food_name}`} className='food-pic' />}
+            <h1 className='food-name'>{post.food_name}</h1>
+            <div className='star-rating'>
+                <StaticStarRating rating={post.rating} />
+            </div>
+            {post.review && post.review.trim() !== '' && <p className='detail-text'>{post.review}</p>}
+            {post.restaurant_name && post.restaurant_name.trim() !== '' && <p className='detail-text'><strong>Restaurant:</strong> {post.restaurant_name}</p>}
+            {post.tags && post.tags.trim() !== '' && <p className='detail-text'><strong>Tags:</strong> {post.tags}</p>}
+            <div id="button">
+                <button className='return' onClick={handleReturnClick}>
+                    Return to Home
+                </button>
             </div>
         </div>
     );
