43b670f8f21336a6d27c6fa3bb2519e9a5875454
Raul Martinez Beltran <ramartin@ucsd.edu>
2024-06-06 14:46:28 -0700

    Fixed merge conflict issues

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,1 +1,56 @@
+import React from 'react';
+import Cookies from 'js-cookie';
+import NavBar from '../../components/navbar'
+import minimizedPost from '../../components/minimizedPost';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from 'react';
+import MinimizedPost from '../../components/minimizedPost';
 
+export default function Profile() {
+    const [pictureNavbar, setPictureNavbar] = useState('http://ssl.gstatic.com/accounts/ui/avatar_2x.png')
+    const [userId, setUserId] = useState(0);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const accessToken = Cookies.get('accessToken');
+
+        if (accessToken) {
+            const url = `https://foodjournal20-production.up.railway.app/api/v1/auth/get_user?accessToken=${accessToken}`;
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                setUserId(data.id)
+            })
+        }
+
+        if (user.id != 0) {
+            const url = `https://foodjournal20-production.up.railway.app/api/v1/post_review/get_posts_by_id?post_id=${userId}`;
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                setPosts(data)
+            })
+        }
+        
+    }, [userId, posts]);
+
+    //const { user } = useAuth();
+
+    //const listItems = posts.map((post) =>);
+
+    return(
+        <div className="Profile">
+
+        </div>
+    );
+}
