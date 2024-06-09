d6502f247d2b54dfc7dfcb17424c6ccd95feab07
ceciliatruong <1ceciliatruong@gmail.com>
2024-06-09 10:35:41 -0700

    sign in & sign up tests

---
+++ frontend/cypress.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
